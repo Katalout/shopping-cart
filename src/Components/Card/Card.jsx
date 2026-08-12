ca69331e768736897f77774d53c025091f43025d
Katalout <253144538+Katalout@users.noreply.github.com>
2026-08-12 19:24:43 +0200

    Add number input to shopping item cards
    (can type in number of items to add)

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,29 +1,53 @@
 import styles from "./Card.module.css"
 import { useOutletContext } from "react-router";
+import { useRef, useState } from "react";
 
 export default function Card({ data }) {
     const [cart, setCart] = useOutletContext();
     const count = cart[data.id] ? cart[data.id].count : null;
+    const input = useRef(null);
+    const [inputVal, setInputVal] = useState('');
+
+    let current = inputVal === "" ? 0 : parseInt(inputVal);
+
+    const incrementVal = () => {
+        setInputVal((current + 1).toString())
+    }
+    const decrementVal = () => {
+        if (current > 0) {
+            let newValue = (current - 1).toString();
+            setInputVal(newValue);
+        }
+    }
 
     const addItemToCart = (item) => () => {
-        let id = item.id;
-        let newCart = { ...cart };
-        if (newCart[id]) newCart[id].count++
-        else newCart[id] = { ...item, count: 1 };
-        setCart(newCart);
+        if (current > 0) {
+            let id = item.id;
+            let newCart = { ...cart };
+            if (newCart[id]) newCart[id].count += current;
+            else newCart[id] = { ...item, count: current };
+            setCart(newCart);
+            setInputVal("");
+        }
     }
-    const removeItemFromCart = (item) => () => {
+    /* const removeItemFromCart = (item) => () => {
         let id = item.id;
         let newCart = { ...cart };
         if (newCart[id].count > 1) newCart[id].count--
         else delete newCart[id];
         setCart(newCart);
+    } */
+
+    const onChange = (e) => {
+        setInputVal(e.target.value);
     }
 
     return <div className={styles.card} >
         <h3>{data.name}</h3>
         <p>{data.price} peták</p>
-        {count && <button onClick={removeItemFromCart(data)}>-</button>}<button onClick={addItemToCart(data)}>+</button>
+        <label htmlFor={data.id}>Item counter: </label><button onClick={decrementVal}>-</button><input ref={input} className={styles.input} type="number" name="count" id={data.id} onChange={onChange} value={inputVal} />
+        <button onClick={incrementVal}>+</button>
+        <button onClick={addItemToCart(data)} disabled={current === 0} >Add to cart</button>
         {count && <p>in cart: {count}</p>}
     </div>
 }
