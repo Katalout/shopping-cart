a410869f78dc4a8add6d9156324e2e10dcddb344
Katalout <253144538+Katalout@users.noreply.github.com>
2026-08-18 12:08:32 +0200

    Animate cartcount with own function (settimeout)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Outlet } from "react-router";
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar.jsx";
@@ -7,13 +7,22 @@ import { data, useProducts } from "./Components/utility.js";
 
 function App() {
   const { products, error, loading } = useProducts();
-
+  const cartRef = useRef(null)
   const [cart, setCart] = useState({});
+
+  function nudge() {
+    cartRef.current.classList.add("animation");
+    console.log("törtenes", cartRef.current);
+    setTimeout(() => {
+      cartRef.current.classList.remove("animation");
+      console.log("removed", cartRef.current)
+    }, 1500)
+  }
 
   return (
     <>
-      <Navbar cart={cart} />
-      <Outlet context={{ cart, setCart, products, error, loading }} />
+      <Navbar cart={cart} ref={cartRef} />
+      <Outlet context={{ cart, setCart, products, error, loading, nudge }} />
     </>)
 }
 
