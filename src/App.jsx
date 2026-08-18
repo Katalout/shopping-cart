b0a50d993549bb0814674be5c8f5fc4879efb9b4
Katalout <253144538+Katalout@users.noreply.github.com>
2026-08-18 13:16:28 +0200

    Animate cartcount without useref, trigger on content change

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,12 @@ import { data, useProducts } from "./Components/utility.js";
 
 function App() {
   const { products, error, loading } = useProducts();
-  const cartRef = useRef(null)
   const [cart, setCart] = useState({});
-
-  function nudge() {
-    cartRef.current.classList.add("animation");
-    console.log("törtenes", cartRef.current);
-    setTimeout(() => {
-      cartRef.current.classList.remove("animation");
-      console.log("removed", cartRef.current)
-    }, 1500)
-  }
 
   return (
     <>
-      <Navbar cart={cart} ref={cartRef} />
-      <Outlet context={{ cart, setCart, products, error, loading, nudge }} />
+      <Navbar cart={cart} />
+      <Outlet context={{ cart, setCart, products, error, loading }} />
     </>)
 }
 
