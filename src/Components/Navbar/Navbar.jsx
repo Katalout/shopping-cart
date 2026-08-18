6443d2a3d20903846e50badf28df2cc51330b04e
Katalout <253144538+Katalout@users.noreply.github.com>
2026-08-18 12:08:32 +0200

    Animate cartcount with own function (settimeout)

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,8 @@
 import { Link } from "react-router";
 import styles from "./Navbar.module.css"
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
-export default function Navbar({ cart }) {
-    //calculate itemscount
+export default function Navbar({ cart, ref }) {
     function countItems() {
         let count = 0;
         Object.values(cart).forEach(item => count += item.count);
@@ -11,6 +10,15 @@ export default function Navbar({ cart }) {
     }
 
     let cartCount = countItems();
+
+    const counter = useRef(null);
+
+    const [animation, setAnimation] = useState(false);
+
+    //calculate itemscount
+
+
+
 
     return (
         <nav>
@@ -22,9 +30,13 @@ export default function Navbar({ cart }) {
                     <Link to="shopping">Shopping</Link>
                 </li>
                 <li>
-                    <Link to="cart">Cart {cartCount > 0 && <span className={styles.cartCount}>{cartCount}</span>}</Link>
+                    <Link to="cart">Cart
+                        <span ref={ref} className={styles.cartCount} >
+                            {cartCount > 0 && cartCount}</span>
+                    </Link>
                 </li>
             </ul>
         </nav>
     )
 }
+// 
