990c02c93509d9d0fda88731a2c1d8f2c85858be
Katalout <253144538+Katalout@users.noreply.github.com>
2026-08-21 12:44:38 +0200

    Add fonts, tweak cards transition

--- src/Components/CategoryCard/CategoryCard.jsx
+++ src/Components/CategoryCard/CategoryCard.jsx
@@ -2,10 +2,10 @@ import styles from "./CategoryCard.module.css"
 
 function CategoryCard({ title, img }) {
     return (
-        <div className={styles.card}>
-            <img className={styles.cardImg} src={img} alt="" />
-            <h3 className={styles.title}>{title}</h3>
-            <div className={styles.overlay}></div>
+        <div className={styles.card} style={{ backgroundImage: `url(${img})` }}>{title}
+            {/* <img className={styles.cardImg} src={img} alt="" /> */}
+            {/* <h3 className={styles.title}></h3> */}
+            {/* <div className={styles.overlay}></div> */}
         </div>
     )
 }
