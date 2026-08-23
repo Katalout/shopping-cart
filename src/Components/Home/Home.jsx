7f7fccf7633eadab47651dbc3b33273f2a934fec
Katalout <253144538+Katalout@users.noreply.github.com>
2026-08-23 12:06:17 +0200

    Style "why us" section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,13 +3,17 @@ import clothes from "../../assets/clothes.jpg";
 import jewellery from "../../assets/jewellery.jpg";
 import electronics from "../../assets/electronics.jpg";
 import styles from "./Home.module.css";
+import diamond from "../../assets/diamond.svg";
+import shield from "../../assets/shield.svg";
+import star from "../../assets/star.svg";
+import approval from "../../assets/approval.svg";
 import CategoryCard from "../CategoryCard/CategoryCard.jsx";
 import { Link } from "react-router";
 
 
 export default function Home() {
 
-
+    console.log(star);
 
     return (
         <>
@@ -33,7 +37,30 @@ export default function Home() {
             </section>
             <section className="whyus">
                 <p className={styles.sectionLabel}>Why us?</p>
-                <h2 className={styles.sectionTitle}>Designed to spark joy and last a lifetime</h2>
+                <h2 className={styles.sectionTitle}>Designed to spark joy and last a lifetime.</h2>
+                <div className={styles.cardWrapper}>
+                    <div className={styles.card}>
+                        <img src={diamond} alt="" />
+                        <h3>Finest materials</h3>
+                        <p>Our products are made locally, from carefully selected and tested materials. Any returns are also recycled to reduce waste.</p>
+                    </div>
+                    <div className={styles.card} >
+                        <img src={star} alt="" />
+                        <h3>Honest reviews</h3>
+                        <p>Every review is from a confirmed purchase. We pride ourselves in the satisfaction of our customers.</p>
+                    </div>
+                    <div className={styles.card}>
+                        <img src={approval} alt="" />
+                        <h3>Transparent return policy</h3>
+                        <p>Should you have any concern, please reach our to customer service. We will find the best solution, be it replacement or repair.</p>
+                    </div>
+                    <div className={styles.card}>
+                        <img src={shield} alt="" />
+                        <h3>Warranty for life</h3>
+                        <p>The quality speaks for itself. We make products to last, so everything is repairable, every part is replaceable.</p>
+                    </div>
+                </div>
+
             </section>
         </>)
 }
